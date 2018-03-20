625df5a506ec077fe932d74980218cae664c62d6
VladislavSheptunov <sheptunov.vld@gmail.com>
2018-03-20 21:26:56 +0300

    Minor changes

--- Polynomial/polynomial.py
+++ Polynomial/polynomial.py
@@ -19,17 +19,17 @@ def RemoveFraction(monom, number_monom):
             str = monom
     return str
 
-def FormMonom(coeff, frac):
-    if coeff < 0:
-        if abs(coeff) == 1:
-            str_tmp = ' - ' + 'x'
-        else:
-            str_tmp = ' - ' + str(abs(round(coeff, frac))) + 'x'
-    elif coeff > 0:
-        if abs(coeff) == 1:
-            str_tmp = ' + ' + 'x'
-        else:
-            str_tmp = ' + ' + str(abs(round(coeff, frac))) + 'x'
+def SetItem(_coeffs, frac, sig):
+    if abs(_coeffs) == 1:
+        return sig + 'x'
+    else:
+        return sig + str(abs(round(_coeffs, frac))) + 'x'
+
+def FormMonom(_coeffs, frac):
+    if _coeffs < 0:
+        return SetItem(_coeffs, frac, '-')
+    elif _coeffs > 0:
+        return SetItem(_coeffs, frac, '+')
     else:
         str_tmp = "empty"
     return str_tmp
@@ -37,9 +37,9 @@ def FormMonom(coeff, frac):
 def GetMonom(monom_number, coeff, code, frac):
     if monom_number == 0:
         if coeff < 0:
-            str_tmp = ' - ' + str(abs(round(coeff, frac)))
+            str_tmp = '-' + str(abs(round(coeff, frac)))
         elif coeff > 0:
-            str_tmp = ' + ' + str(abs(round(coeff, frac)))
+            str_tmp = '+' + str(abs(round(coeff, frac)))
         else:
             str_tmp = "empty"
     if monom_number == 1:
@@ -58,12 +58,8 @@ def GetMonom(monom_number, coeff, code, frac):
 
 def EditSeniorMonom(arg):
     ret_str = str()
-    arg.pop(0)
     if arg[0] == '+':
         arg.pop(0)
-        arg.pop(0)
-    else:
-        arg.pop(1)
     for it in arg:
         ret_str += str(it)
     return ret_str
@@ -74,16 +70,16 @@ def GetPolinomial(list_monom):
         ret_str += str(list_monom[it])
     return ret_str
 
-def IsFloat(s):
+def IsNumerical(s):
     try:
         float(s)
         return True
     except ValueError:
         return False
 
-def ToFloat(coeff):
+def ToFloat(_coeffs):
     tmp = []
-    for it in coeff:
+    for it in _coeffs:
         tmp.append(float(it))
     return tmp
 
@@ -93,16 +89,16 @@ class Polynomial:
             '6': chr(8310), '7': chr(8311), '8': chr(8312),
             '9': chr(8313)}
 
-    def __init__(self, coeff = 0):
-        self._coeff = list()
-        self.__checkValue(coeff)
-        self.__addValue(coeff)
+    def __init__(self, _coeffs = 0):
+        self.coeffs = list()
+        self.__checkValue(_coeffs)
+        self.__addValue(_coeffs)
 
     def __repr__(self):
-       if len(self._coeff) == 1:
-            return 'Polynomial({})'.format(self._coeff[0])
+       if len(self.coeffs) == 1:
+            return 'Polynomial({})'.format(self.coeffs[0])
        else:
-           tmp_list = self._coeff
+           tmp_list = self.coeffs
            for it in reversed(tmp_list):
                if it == 0:
                    tmp_list.remove(it)
@@ -112,10 +108,10 @@ class Polynomial:
 
     def __str__(self):
         list_monom = list()
-        len_poly = len(self._coeff)
-        if len_poly > 1 and len_poly - self._coeff.count(0) != 0:
+        len_poly = len(self.coeffs)
+        if len_poly > 1 and len_poly - self.coeffs.count(0) != 0:
             for monom_number in range(0, len_poly, 1):
-                tmp_str = GetMonom(monom_number, self._coeff[monom_number], self.code, 3)
+                tmp_str = GetMonom(monom_number, self.coeffs[monom_number], self.code, 3)
                 if tmp_str != "empty":
                     list_monom.append(tmp_str)
                 else:
@@ -128,10 +124,10 @@ class Polynomial:
 
     def __eq__(self, other):
         other = Polynomial(other)
-        if len(self._coeff) != len(other._coeff):
+        if len(self.coeffs) != len(other.coeffs):
             return False
-        zip(self._coeff, other._coeff)
-        for i, j in zip(self._coeff, other._coeff):
+        zip(self.coeffs, other.coeffs)
+        for i, j in zip(self.coeffs, other.coeffs):
             if i != j:
                 return False
         return True
@@ -144,124 +140,124 @@ class Polynomial:
             return False;
 
     def __neg__(self):
-        resPolynomial = Polynomial(self._coeff)
-        for it in range(0, len(resPolynomial._coeff),1):
-            resPolynomial._coeff[it] *= -1
+        resPolynomial = Polynomial(self.coeffs)
+        for it in range(0, len(resPolynomial.coeffs), 1):
+            resPolynomial.coeffs[it] *= -1
         return resPolynomial
 
     def __pos__(self):
-        resPolynomial = Polynomial(self._coeff)
-        for it in range(0, len(resPolynomial._coeff),1):
-            resPolynomial._coeff[it] = abs(resPolynomial._coeff[it])
+        resPolynomial = Polynomial(self.coeffs)
+        for it in range(0, len(resPolynomial.coeffs), 1):
+            resPolynomial.coeffs[it] = abs(resPolynomial.coeffs[it])
         return resPolynomial
 
     def __bool__(self):
-        if len(self._coeff) == 1 and self._coeff[0] == 0:
+        if len(self.coeffs) == 1 and self.coeffs[0] == 0:
             return False
         else:
             return True
 
     def __iadd__(self, other):
         other = Polynomial(other)
-        len_poly = len(other._coeff)
-        if len(self._coeff) <= len_poly:
-            for it in range(len(self._coeff),len(self._coeff) + (len_poly - len(self._coeff)), 1):
-                self._coeff.append(0)
+        len_poly = len(other.coeffs)
+        if len(self.coeffs) <= len_poly:
+            for it in range(len(self.coeffs), len(self.coeffs) + (len_poly - len(self.coeffs)), 1):
+                self.coeffs.append(0)
         for it in range(0,len_poly,1):
-            self._coeff[it] += other._coeff[it]
-            tmp = self._coeff[it]
-            if tmp - self._coeff[it] == 0:
-                int(self._coeff[it])
+            self.coeffs[it] += other.coeffs[it]
+            tmp = self.coeffs[it]
+            if tmp - self.coeffs[it] == 0:
+                int(self.coeffs[it])
         return self
 
     def __add__(self, other):
         other = Polynomial(other)
-        resPolynomial = Polynomial(self._coeff)
+        resPolynomial = Polynomial(self.coeffs)
         resPolynomial += other
         return resPolynomial
 
     def __radd__(self, other):
         other = Polynomial(other)
-        resPolynomial = Polynomial(self._coeff)
+        resPolynomial = Polynomial(self.coeffs)
         other += resPolynomial
         return other
 
     def __isub__(self, other):
         other = Polynomial(other)
-        len_poly = len(other._coeff)
-        if len(self._coeff) <= len_poly:
-            for it in range(len(self._coeff),len(self._coeff) + (len_poly - len(self._coeff)), 1):
-                self._coeff.append(0)
+        len_poly = len(other.coeffs)
+        if len(self.coeffs) <= len_poly:
+            for it in range(len(self.coeffs), len(self.coeffs) + (len_poly - len(self.coeffs)), 1):
+                self.coeffs.append(0)
         for it in range(0,len_poly,1):
-            self._coeff[it] -= other._coeff[it]
+            self.coeffs[it] -= other.coeffs[it]
         return self
 
     def __sub__(self, other):
         other = Polynomial(other)
-        resPolynomial = Polynomial(self._coeff)
+        resPolynomial = Polynomial(self.coeffs)
         resPolynomial -= other
         return resPolynomial
 
     def __rsub__(self, other):
         other = Polynomial(other)
-        resPolynomial = Polynomial(self._coeff)
+        resPolynomial = Polynomial(self.coeffs)
         other -= resPolynomial
         return other
 
     def __imul__(self, other):
         other = Polynomial(other)
-        len_poly = len(self._coeff) + len(other._coeff)
+        len_poly = len(self.coeffs) + len(other.coeffs)
         resPolynomial = Polynomial([0 for i in range(len_poly)])
-        for i in range(0, len(self._coeff), 1):
-            for j in range(0, len(other._coeff), 1):
-                resPolynomial._coeff[i + j] += self._coeff[i] * other._coeff[j]
-        self._coeff = []
-        for it in resPolynomial._coeff:
-            self._coeff.append(it)
+        for i in range(0, len(self.coeffs), 1):
+            for j in range(0, len(other.coeffs), 1):
+                resPolynomial.coeffs[i + j] += self.coeffs[i] * other.coeffs[j]
+        self.coeffs = []
+        for it in resPolynomial.coeffs:
+            self.coeffs.append(it)
         return self
 
 
     def __mul__(self, other):
         other = Polynomial(other)
-        resPolynomial = Polynomial(self._coeff)
+        resPolynomial = Polynomial(self.coeffs)
         resPolynomial *= other
         return resPolynomial
 
     def __rmul__(self, other):
         other = Polynomial(other)
-        resPolynomial = Polynomial(self._coeff)
+        resPolynomial = Polynomial(self.coeffs)
         other *= resPolynomial
         return other
 
-    def __addValue(self, coeff):
-        if (type(coeff) is int) or (type(coeff) is float):
-            self._coeff.append(float(coeff))
-        elif type(coeff) is Polynomial:
-            self._coeff = ToFloat(coeff._coeff)
-        elif type(coeff) is str:
-            tmp_str = re.sub('[\,]', '', coeff).split(' ')
+    def __addValue(self, _coeff):
+        if (type(_coeff) is int) or (type(_coeff) is float):
+            self.coeffs.append(float(_coeff))
+        elif type(_coeff) is Polynomial:
+            self.coeffs = ToFloat(_coeff.coeffs)
+        elif type(_coeff) is str:
+            tmp_str = re.sub('[\,]', '', _coeff).split(' ')
             for it in range(0,len(tmp_str),1):
-                if IsFloat(tmp_str[it]):
-                    self._coeff.append(float(tmp_str[it]))
+                if IsNumerical(tmp_str[it]):
+                    self.coeffs.append(float(tmp_str[it]))
         else:
-            self._coeff = ToFloat(list(coeff))
+            self.coeffs = ToFloat(list(_coeff))
 
-    def __checkValue(self, coeff):
-        if type(coeff) is str:
-            if len(coeff) == 1 and IsFloat(coeff[0]) == False:
+    def __checkValue(self, _coeff):
+        if type(_coeff) is str:
+            if len(_coeff) == 1 and IsNumerical(_coeff[0]) == False:
                 raise ValueError("Invalid initialization list! Not a number!")
-            if (len(coeff) > 1):
-                tmp_checkList = re.sub('[\,]', '', coeff).split(' ')
+            if (len(_coeff) > 1):
+                tmp_checkList = re.sub('[\,]', '', _coeff).split(' ')
                 for it in tmp_checkList:
-                    if IsFloat(it) == False:
+                    if IsNumerical(it) == False:
                         raise ValueError("Invalid initialization list! Not a number!")
-        if (type(coeff) is list) or (type(coeff) is tuple):
-            for it in coeff:
+        if (type(_coeff) is list) or (type(_coeff) is tuple):
+            for it in _coeff:
                 if (type(it) is not int) and (type(it) is not float):
                     raise ValueError("Error! Invalid type in initialization list")
 
     def __del__(self):
-        del self._coeff
+        del self.coeffs
 
 
 if __name__ == "__main__":
--- Polynomial/utest.py
+++ Polynomial/utest.py
@@ -21,29 +21,45 @@ class TestClassPolynomial(unittest.TestCase):
 
     # @unittest.skip('Temporarily disabled')
     def test_method__str__(self):
-        self.test_srt = "10x¹⁰ + 9x⁹ + 8x⁸ + 7x⁷ + 6x⁶ + 5x⁵ + 4x⁴ + 3x³ + 2x² + x"
+        self.test_srt = "10x¹⁰+9x⁹+8x⁸+7x⁷+6x⁶+5x⁵+4x⁴+3x³+2x²+x"
         self.tp_A = pm.Polynomial(range(11))
         self.assertEqual(self.tp_A.__str__(), self.test_srt)
 
-        self.test_srt = "-x² + 0x + 1.234"
+        self.test_srt = "-x²+0x+1.234"
         self.tp_B = pm.Polynomial((1.2345, 0.0001, -1))
         self.assertEqual(self.tp_B.__str__(), self.test_srt)
 
-        self.test_srt = "x⁶ - x⁴ + x² + 1"
+        self.test_srt = "x⁶-x⁴+x²+1"
         self.tp_C = pm.Polynomial((1, 0, 1, 0, -1, 0, 1))
         self.assertEqual(self.tp_C.__str__(), self.test_srt)
 
+        self.test_srt = "-1.1x²+5.5x-1"
+        self.tp_B = pm.Polynomial((1.1, 5.5, -1))
+        self.assertNotEqual(self.tp_B.__str__(), self.test_srt)
+
+        self.test_srt = "x²+1x-1"
+        self.tp_B = pm.Polynomial((-1, 1, 1))
+        self.assertNotEqual(self.tp_B.__str__(), self.test_srt)
+
     # @unittest.skip('Temporarily disabled')
     def test_method__neg__(self):
-        self.test_srt = "-4x⁶ - 1.22x⁵ - 44x³ - 2.45x + 1"
+        self.test_srt = "-4x⁶-1.22x⁵-44x³-2.45x+1"
         self.tp_A = pm.Polynomial('-1, 2.45, 0, 44 0, 1.22, 4')
         self.assertEqual((-self.tp_A ).__str__(), self.test_srt)
 
+        self.test_srt = "-x⁵-x³-2x"
+        self.tp_A = pm.Polynomial('0, -2, 0, 1 0, -1')
+        self.assertNotEqual((-self.tp_A ).__str__(), self.test_srt)
+
     # @unittest.skip('Temporarily disabled')
     def test_method__pos__(self):
-        self.test_srt = "4x⁶ + 1.22x⁵ + 44x³ + 2.45x + 1"
+        self.test_srt = "4x⁶+1.22x⁵+44x³+2.45x+1"
         self.tp_A = pm.Polynomial('-1, 2.45, 0, 44 0, -1.22, 4')
         self.assertEqual((+self.tp_A).__str__(), self.test_srt)
+
+        self.test_srt = "-x⁵-x³-2x"
+        self.tp_A = pm.Polynomial('0, 2, 0, -1 0, 1')
+        self.assertNotEqual((-self.tp_A ).__str__(), self.test_srt)
 
     # @unittest.skip('Temporarily disabled')
     def test_method__bool__(self):
@@ -54,51 +70,51 @@ class TestClassPolynomial(unittest.TestCase):
 
     # @unittest.skip('Temporarily disabled')
     def test_method__add__(self):
-        self.test_srt = "4x⁴ - 2x³ + 2x² + 3.34x + 1.23"
+        self.test_srt = "4x⁴-2x³+2x²+3.34x+1.23"
         self.assertEqual((self.tp_A + self.tp_B).__str__(), self.test_srt)
 
     # @unittest.skip('Temporarily disabled')
     def test_method__radd__(self):
-        self.test_srt = "-5x³ + 2.34x + 3.33"
+        self.test_srt = "-5x³+2.34x+3.33"
         self.assertEqual((2.1 + self.tp_B).__str__(), self.test_srt)
 
     # @unittest.skip('Temporarily disabled')
     def test_method__iadd__(self):
-        self.test_srt = "-5x³ + 2x² + 2.34x + 2"
+        self.test_srt = "-5x³+2x²+2.34x+2"
         self.tp_A = pm.Polynomial((1.23, 2.34, 0, -5))
         self.tp_A += (0.77, 0, 2)
         self.assertEqual((self.tp_A).__str__(), self.test_srt)
 
     # @unittest.skip('Temporarily disabled')
     def test_method__sub__(self):
-        self.test_srt = "4x⁴ + 8x³ + 2x² - 1.34x - 1.23"
+        self.test_srt = "4x⁴+8x³+2x²-1.34x-1.23"
         self.assertEqual((self.tp_A - self.tp_B).__str__(), self.test_srt)
 
     # @unittest.skip('Temporarily disabled')
     def test_method__rsub__(self):
-        self.test_srt = "5x³ - 2.34x + 0.87"
+        self.test_srt = "5x³-2.34x+0.87"
         self.assertEqual((2.1 - self.tp_B).__str__(), self.test_srt)
 
     # @unittest.skip('Temporarily disabled')
     def test_method__isub__(self):
-        self.test_srt = "-6x³ + 2x² + 2x"
+        self.test_srt = "-6x³+2x²+2x"
         self.tp_B = pm.Polynomial((1.23, 2.34, 0, -5))
         self.tp_B -= (1.23, 0.34, -2, 1)
         self.assertEqual(self.tp_B.__str__(), self.test_srt)
 
     # @unittest.skip('Temporarily disabled')
     def test_method__mul__(self):
-        self.test_srt = "x² - 1"
+        self.test_srt = "x²-1"
         self.tp_A = pm.Polynomial((1, 1))
         self.tp_B = pm.Polynomial((-1, 1))
         self.assertEqual((self.tp_A * self.tp_B).__str__(), self.test_srt)
 
-        self.test_srt = "10x³ - 5x² - 10x"
+        self.test_srt = "10x³-5x²-10x"
         self.tp_A = pm.Polynomial(-5)
         self.tp_B = pm.Polynomial((0, 2, 1, -2))
         self.assertEqual((self.tp_A * self.tp_B).__str__(),  self.test_srt)
 
-        self.test_srt = "-x¹⁰ - 2x⁹ - 3x⁸"
+        self.test_srt = "-x¹⁰-2x⁹-3x⁸"
         self.tp_A = pm.Polynomial((0,0,0,0,0,0,0,0,3,2,1))
         self.tp_B = pm.Polynomial(-1)
         self.assertEqual((self.tp_A * self.tp_B).__str__(), self.test_srt)
@@ -106,21 +122,21 @@ class TestClassPolynomial(unittest.TestCase):
         self.tp_B = 0
         self.assertEqual((self.tp_A * self.tp_B).__str__(), '0')
 
-        self.test_srt = "x¹³ - 4x¹¹ + 5x⁵ - 20x³"
+        self.test_srt = "x¹³-4x¹¹+5x⁵-20x³"
         self.tp_A = pm.Polynomial((0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 1))
         self.tp_B = pm.Polynomial((0, -4, 0, 1))
         self.assertEqual((self.tp_A * self.tp_B).__str__(), self.test_srt)
 
     # @unittest.skip('Temporarily disabled')
     def test_method__rmul__(self):
-        self.test_srt = "-x⁵ + 3x³ + 3x² - 2x - 3"
+        self.test_srt = "-x⁵+3x³+3x²-2x-3"
         self.tp_A = pm.Polynomial((3, 2, 0, -1))
         self.tp_B = pm.Polynomial((-1, 0, 1))
         self.assertEqual((self.tp_B * self.tp_A).__str__(), self.test_srt)
 
     # @unittest.skip('Temporarily disabled')
     def test_method__imul__(self):
-        self.test_srt = "-x⁵ + 3x³ + 3x² - 2x - 3"
+        self.test_srt = "-x⁵+3x³+3x²-2x-3"
         self.tp_A = pm.Polynomial((-1, 0, 1))
         self.tp_A *= (3, 2, 0, -1)
         self.assertEqual((self.tp_A).__str__(), self.test_srt)
